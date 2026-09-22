66c632a7ffdca68bdb59b7fcfab2c1f75a1bf715
Aquila Fernando Alves Silva <aquila.silva@estudante.iftm.edu.br>
2026-09-21 22:18:05 -0300

    Fixes login validation

--- 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/Login.js
+++ 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/Login.js
@@ -7,8 +7,12 @@ const btnEntrar = document.getElementById("btnEntrar");
 btnEntrar.addEventListener("click", logarUsuario);
 
 function logarUsuario(){
+
     userLogin = userNameLogin.value;
     senhaLogin = userPassLogin.value;
+
+    let usuarioLogado = {name: userLogin, pass: senhaLogin};
+
     if(userLogin == "" || senhaLogin == ""){
         txtLogin.innerHTML = "Acesso negado!<br>Preencha todos os Campos"
         txtLogin.style.color = "Red";
@@ -18,24 +22,27 @@ function logarUsuario(){
         if(usuariosLocal === null){
             txtLogin.innerHTML = "Acesso inválido!<br>Nenhum usuario cadastrado"
             txtLogin.style.color = "Red";
+            return;
         }else{
             dadosLocal = JSON.parse(localStorage.getItem("usuariosLDT"));
             
-            resultadoLogin = null;
-            for(let i = 0; i<dadosLocal[i].length; i++){
+            let resultadoLogin = false;
+
+            for(let i = 0; i<dadosLocal.length; i++){
                 
-                if(dadosLocal[i].name == userLogin && dadosLocal[i].pass == senhaLogin){
-                    let usuarioLogado = dadosLocal[i];
+                if(dadosLocal[i].name == usuarioLogado.name && dadosLocal[i].pass == usuarioLogado.pass ){
+                    
                     resultadoLogin = true;
                     break; 
                 }
             }
+
             if(resultadoLogin == false){
                 txtLogin.innerHTML = "Acesso negado!<br>Usuario ou Senha errados";
                 txtLogin.style.color = "Red";
                 return;
             }else{
-                usuarioLogado = {name:userLogin, pass:senhaLogin};
+
                 localStorage.setItem("usuarioLogadoLDT", JSON.stringify(usuarioLogado));
 
                 txtLogin.innerHTML = "Acesso autorizado!<br>Aguarde enquanto te redirecionamos"

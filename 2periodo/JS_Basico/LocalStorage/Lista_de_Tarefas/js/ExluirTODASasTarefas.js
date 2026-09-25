e57b162a2b4f550ca528d53b99c6cb9dbf2e7880
Aquila Fernando Alves Silva <aquila.silva@estudante.iftm.edu.br>
2026-09-25 16:45:51 -0300

    Adds task list clearing

---
+++ 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/ExluirTODASasTarefas.js
@@ -0,0 +1,19 @@
+const btnLimpar = document.getElementById("btnLimpar");
+
+btnLimpar.addEventListener("click", limparTarefas);
+
+function limparTarefas(){
+    if(confirm("Tem certeza que deseja apagar todas as suas tarefas?")){
+        tarefasTotal = JSON.parse(localStorage.getItem("listaDeTarefas"))
+        usuarioAtual = JSON.parse(localStorage.getItem("usuarioLogadoLDT"))
+        tarefasFiltradas = [];
+        for(let i = 0; i<tarefasTotal; i++){
+            if(tarefasTotal[i].usuario != usuarioAtual.name){
+                tarefasFiltradas.push(tarefasTotal[i]);
+            }
+        }
+        alert("Todas as tarefas foram apagadas!")
+        localStorage.setItem("listaDeTarefas", JSON.stringify(tarefasFiltradas));
+        atualizaLista();
+    }
+}

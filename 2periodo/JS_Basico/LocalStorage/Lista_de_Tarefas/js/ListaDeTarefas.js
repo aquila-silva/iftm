2e3c7eac1b9bba8a578835ce92a50a9b32baad23
Aquila Fernando Alves Silva <aquila.silva@estudante.iftm.edu.br>
2026-09-25 17:09:50 -0300

    Improves task list presentation

--- 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/ListaDeTarefas.js
+++ 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/ListaDeTarefas.js
@@ -23,9 +23,20 @@ function atualizaLista(){
     }else{
         tafCadastrada.style.display = "flex";
         tafVazia.style.display = "none";
+
         listaTAF.innerHTML = "";
+        
         for(let i = 0; i<listaFiltrada.length; i++){
-            listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`
+            if(listaFiltrada[i].propriedade == "Alta")
+                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`
+        }
+        for(let i = 0; i<listaFiltrada.length; i++){
+            if(listaFiltrada[i].propriedade == "Moderada")
+                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`
+        }
+        for(let i = 0; i<listaFiltrada.length; i++){
+            if(listaFiltrada[i].propriedade == "Baixa")
+                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`
         }
 
     }

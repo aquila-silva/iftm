1cd11384fb44c0e6eae7a33f41978b668efb1fa5
Aquila Fernando Alves Silva <aquila.silva@estudante.iftm.edu.br>
2026-09-25 11:22:36 -0300

    Improves welcome message

--- 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/PersonalizacaoIndex.js
+++ 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/PersonalizacaoIndex.js
@@ -5,6 +5,6 @@ carregaApelido();
 function carregaApelido(){
     usuariologado = JSON.parse(localStorage.getItem("usuarioLogadoLDT"));
 
-    hBV.innerHTML = `Que bom ver você por aqui <br>${usuariologado.apelido}`
+    hBV.innerHTML = `Que bom ver você por aqui <br> <b> ${usuariologado.apelido} </b>`
 
 }

68edfc664249163a993917dcbe48258be450e280
Aquila Fernando Alves Silva <aquila.silva@estudante.iftm.edu.br>
2026-09-25 17:21:46 -0300

    Improves task priority ordering

--- 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/ListaDeTarefas.js
+++ 2periodo/JS_Basico/LocalStorage/Lista_de_Tarefas/js/ListaDeTarefas.js
@@ -25,18 +25,20 @@ function atualizaLista(){
         tafVazia.style.display = "none";
 
         listaTAF.innerHTML = "";
-        
+
         for(let i = 0; i<listaFiltrada.length; i++){
-            if(listaFiltrada[i].propriedade == "Alta")
-                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`
+            if(listaFiltrada[i].prioridade == "Alta"){
+                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`;
+            }
         }
         for(let i = 0; i<listaFiltrada.length; i++){
-            if(listaFiltrada[i].propriedade == "Moderada")
-                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`
-        }
-        for(let i = 0; i<listaFiltrada.length; i++){
-            if(listaFiltrada[i].propriedade == "Baixa")
-                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`
+            if(listaFiltrada[i].prioridade == "Moderada"){
+                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`;
+            }
+        }for(let i = 0; i<listaFiltrada.length; i++){
+            if(listaFiltrada[i].prioridade == "Baixa"){
+                listaTAF.innerHTML += `<li><b>Tarefa:</b> ${listaFiltrada[i].nome} <b>Prazo: </b> ${listaFiltrada[i].prazo} <b>Prioridade: </b> ${listaFiltrada[i].prioridade}</li>`;
+            }
         }
 
     }
